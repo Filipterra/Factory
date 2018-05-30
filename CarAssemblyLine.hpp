126a42f3783adfbb3e3525bc8f325ba15d35d4a1
Filipterra <37516675+Filipterra@users.noreply.github.com>
2018-05-30 11:43:53 +0200

    Add files via upload

--- CarAssemblyLine.hpp
+++ CarAssemblyLine.hpp
@@ -42,5 +42,47 @@ class bodyElectronics: bodyEngine {
 	virtual ~bodyElectronics();
 };
 
+template <class C> class conveyor : public edge {
+C element;
 
+public:
+conveyor (): amt(0) {};
+virtual ~conveyor(){};
+
+virtual int size () const noexcept {return amt;};
+virtual void take() noexcept {--amt;};
+virtual void add() noexcept {++amt;};
+};
+
+template <class C> class productionSite : public node {
+virtual bool space();
+//virtual bool materials ();
+
+public:
+virtual ~productionSite() {};
+virtual void run();
+};
+
+template <class C> class assemblySite : public node {
+virtual bool space();
+virtual bool materials ();
+
+public:
+virtual ~assemblySite() {};
+virtual void run();
+};
+
+template <> class productionSite<tire> : public node{
+virtual bool space() {
+
+}
+
+public:
+virtual void run() {
+if (this->space())
+{
+
+}
+}
+};
 #endif /* CARASSEMBLYLINE_HPP_ */
